391091148e31fa9413d1b8ca86d3de8c63f216d2
Alexander Vorontsov <108517482+alexandrvorontsov@users.noreply.github.com>
2023-09-10 09:33:14 +0300

    change  width: 35%;

--- src/components/FriendList/FriendList.styled.jsx
+++ src/components/FriendList/FriendList.styled.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const FriendListBox = styled.ul`
-  width: 30%;
+  width: 35%;
   display: flex;
   flex-direction: column;
   margin: 0 auto 15px auto;
